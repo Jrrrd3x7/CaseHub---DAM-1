8818cba8bd43fef6fd7f652c02f74a4eb0599a44
Daniel Lop <daniellopbarbara@gmail.com>
2026-03-25 08:39:26 +0100

    CreateUsers y Database

--- model/CaseHUB.sql
+++ model/CaseHUB.sql
@@ -3,35 +3,60 @@
 -- Jordi Baez, Ivan Forner, Daniel Lop, Eduard Caringal -- 
 -- 05/12/2025 (Formato: DD/MM/AAAA) --
 
--- Creacion tablas Clientes --
-create database IF NOT EXISTS casehub;
-USE casehub;
-CREATE TABLE Clientes(
-id_cliente int auto_increment,
-nombre_cliente varchar(100) not null,
-numero_telefono int default 11,
-email varchar (100) not null,
-fecha_nacimiento varchar(8),
-pais varchar(100),
-provincia varchar(100),
-direccion varchar(200),
-codigo_postal int default 15,
-numero_tarjeta int,
-contraseña varchar(50) not null,
-constraint PK_Clientes primary key (id_cliente)
+-- CREATE DATABASE CASEHUB --
+CREATE DATABASE IF NOT EXISTS CaseHub;
+USE CaseHub;
+
+-- RESET DATABASE --
+DROP TABLE IF EXISTS Users;
+
+DROP PROCEDURE IF EXISTS PrintUsers;
+DROP PROCEDURE IF EXISTS CreateUser;
+
+CREATE TABLE Users(
+	ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	Name VARCHAR(100) NOT NULL,
+	Surname VARCHAR(100) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	Country VARCHAR(100) NOT NULL,
+	Password VARCHAR(50) NOT NULL
 );
+INSERT INTO Users(Name,Surname,Email,Country,Password)
+VALUES
+('Jordi','Messi','jordi@email.com','España','jordi123');
+
+-- PRINT USERS -- 
+DELIMITER $$
+CREATE PROCEDURE PrintUsers()
+BEGIN
+    SELECT
+		Users.ID AS ID,
+        Users.Name AS Name,
+        Users.Surname AS Surname,
+        Users.Email AS Email,
+        Users.Country AS Country,
+        Users.Password AS Password
+    FROM Users;
+END $$
+DELIMITER ;
+
+-- CREATE USER --
+DELIMITER $$
+	CREATE PROCEDURE CreateUser (
+		IN pName VARCHAR(100), 
+		IN pSurname VARCHAR(100), 
+		IN pEmail VARCHAR(100),
+		IN pCountry VARCHAR(100),
+		IN pPassword VARCHAR(100)
+	)
+	BEGIN
+		INSERT INTO Users(Name,Surname,Email,Country,Password)
+		VALUES (pName,pSurname,pEmail,pCountry,pPassword);
+	END $$
+DELIMITER ;
 
 
--- Creacion tablas Suscripciones --
-use casehub;
-CREATE TABLE Suscripciones (
-    id_suscripcion int primary key auto_increment,
-    id_cliente int not null,
-    tipo_suscripcion varchar(50) not null,
-    fecha_inicio  varchar(8) not null,
-    fecha_fin varchar(8),
-    CONSTRAINT FK_Suscripciones_Clientes
-	FOREIGN KEY (id_cliente)
-	REFERENCES Clientes(id_cliente)
-	ON DELETE CASCADE
-);
+-- EXECUTE --
+CALL PrintUsers();
+CALL CreateUser('Daniil','Maximov','dmaximov@email.com','Rusia','dmx123');
+CALL PrintUsers();
