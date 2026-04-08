f671dff5e89a0ded9da6020b151d13dfd99a670f
EduardOnnichann <eduard.reineth@gmail.com>
2026-04-08 22:37:14 +0200

    Delete CaseHub---DAM-1-mainInterficiesEduard--definitivo/CaseHub---DAM-1-main directory
    Vale tampoco, me he hecho un lio de carpetas pero se supone que el siguiente si que es

--- CaseHub---DAM-1-mainInterficiesEduard--definitivo/CaseHub---DAM-1-main/view/scripts/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../controller/UserController.php';
-
-$userController = new UserController();
-$userController->logout();
-
-header('Location: ../html/login.html?success=' . urlencode('Sesion cerrada correctamente.'));
-exit();
