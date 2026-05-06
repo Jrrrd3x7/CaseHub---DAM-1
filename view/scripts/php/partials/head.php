b7a394a593c27b8187630128fd4bb69ecc152392
Eduard Caringal <eduard.reineth@gmail.com>
2026-05-06 09:03:24 +0200

    redireccion de links

--- view/scripts/php/partials/head.php
+++ view/scripts/php/partials/head.php
@@ -5,6 +5,7 @@
     <link type="text/css" rel="stylesheet" href="../css/events.css">
     <link type="text/css" rel="stylesheet" href="../css/evento.css">
     <link type="text/css" rel="stylesheet" href="../css/formularios.css">
+    <link type="text/css" rel="stylesheet" href="../css/event-forms.css">
     <link type="text/css" rel="stylesheet" href="../css/phone.css" media="(max-width:1025px)">
     <link type="text/css" rel="stylesheet" href="../css/desktop.css" media="(min-width:1025px)">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
