53efff83f45233cc8743fea3c80f1bd0986e65c6
Eduard Caringal <eduard.reineth@gmail.com>
2026-05-13 12:57:43 +0200

    eventos.sql

--- view/scripts/php/partials/footer.php
+++ view/scripts/php/partials/footer.php
@@ -4,7 +4,7 @@
         <ul class="footer-links">
             <li><a href="index.php">Inicio</a></li>
             <li><a href="events.php">Eventos</a></li>
-            <li><a href="evento1.php">Evento actual</a></li>
+            <li><a href="evento-detail.php?id=1">Evento actual</a></li>
             <li><a href="createEV.php">Crear evento</a></li>
             <li><a href="login.php">Iniciar sesion</a></li>
             <li><a href="account.php">Perfil</a></li>
